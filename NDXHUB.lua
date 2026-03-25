8cb4be0e6d76ca160e8e41aa39ee4fc81f3eedc7
NDXWorkspace <nedethralnean@gmail.com>
2026-03-25 12:16:45 +0700

    Fix URL in loadstring for keysystem.lua

--- NDXHUB.lua
+++ NDXHUB.lua
@@ -80,7 +80,7 @@ local function loadMainScript(info)
     print("============================")
 
     -- TARUH SCRIPT UTAMA KAMU DI SINI:
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/NDXWorkspace/NDXPROJECT/refs/heads/main/keysystem.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/NDXWorkspace/NDXPROJECT/main/keysystem.lua"))()
 end
 
 -- =============================================
